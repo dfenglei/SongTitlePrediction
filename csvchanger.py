98bcb72e98d47bc3a6895a8d71aa09d4117bf555
elenaty4 <elenango1118@yahoo.com>
2017-11-22 18:10:03 -0500

    Update csvchanger.py

--- csvchanger.py
+++ csvchanger.py
@@ -5,7 +5,7 @@ import csv
 # put the titles in the first column of the NEW csv file
 # put the no end line lyrics in the second column of the NEW csv file
 
-csvFile = "songdata.csv"
+csvFile = "../songdata.csv"
 file = open(csvFile, "r")
 csvRead = csv.reader(file, delimiter=",")
 titlesList = []
@@ -21,15 +21,25 @@ newLyricsList = []
 for ii in range(0, len(lyricsList)):
     lyrics = str(lyricsList[ii].lower())
     newLyrics = lyrics.replace('\n', " ").replace(',', " ").replace('.', " ").replace('?', " ").replace('!', " ")\
-        .replace('\'', " ").replace('\"', " ").replace(':', " ").replace(';', " ").replace('(', " ").replace(')', " ")\
+        .replace('\'', "").replace('\"', " ").replace(':', " ").replace(';', " ").replace('(', " ").replace(')', " ")\
         .replace('[', " ").replace(']', " ").replace('_', " ")
     newLyricsList.append(newLyrics)
+    
+newTitlesList = []
+for ii in range(0, len(titlesList)):
+    title = str(titlesList[ii].lower())
+    newTitle = title.replace('\n', " ").replace(',', " ").replace('.', " ").replace('?', " ").replace('!', " ")\
+        .replace('\'', "").replace('\"', " ").replace(':', " ").replace(';', " ").replace('(', " ").replace(')', " ")\
+        .replace('[', " ").replace(']', " ").replace('_', " ")
+    newTitlesList.append(newTitle)
+
+
 # now add them to a new csv file
 newCSV = open("songdatarevised.csv", "w")
 with newCSV:
     writer = csv.writer(newCSV)
-    for jj in range(0, len(newLyricsList)):
-        title = str(titlesList[jj])
+    for jj in range(0, len(newTitlesList)):
+        title = str(newTitlesList[jj])
         songLyric = str(newLyricsList[jj])
         dataRow = [[title, songLyric]]
         writer.writerows(dataRow)
